0c2e289c43a18dc19b77aba7a96c8a42c7cb222a
yazdani <arvin.yazdani1999@gmail.com>
2021-09-16 16:56:10 +0430

    fix basic view and template with pagination and urls , let't go to share post with email

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404,render
+from .models import Post
+from django.views.generic import ListView
 
-# Create your views here.
+class PostListView(ListView):
+    #model = Post
+    queryset = Post.publishedmanager.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+def post_detail(request,year,month,day,post):
+    post = get_object_or_404(Post,slug=post,status='published',\
+        published__year=year,published__month=month,\
+            published__day=day)
+    context = {
+        "post":post
+    }    
+    return render(request, 'blog/post/detail.html', context)    
+
+
